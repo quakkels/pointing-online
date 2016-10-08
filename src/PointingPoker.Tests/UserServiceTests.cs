c688fc6b09069d2b13556afe329007dd2f234f82
quakkels <quakkels@gmail.com>
2016-10-08 13:20:30 -0500

    Added testing project

---
+++ src/PointingPoker.Tests/UserServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using PointingPoker.DataAccess;
+using PointingPoker.DataAccess.Models;
+using PointingPoker.DataAccess.Queries;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PointingPoker.Tests
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public void CanGetUsers()
+        {
+            // arrange
+            var userQueriesMock = new Mock<IUserQueries>();
+            userQueriesMock.Setup(x => x.GetUsers())
+                .Returns(new List<User>
+                {
+                    new User{
+                        Id = Guid.NewGuid(),
+                        Username = "bob",
+                        Email = "email@email"
+                    }
+                });
+            var userService = new UserService(userQueriesMock.Object);
+
+            // act
+            var result = userService.GetUsers() as List<User>;
+
+            // assert
+            Assert.Equal("bob", result[0].Username);
+        }
+    }
+}
