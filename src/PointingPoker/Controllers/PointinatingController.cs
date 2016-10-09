2103c03c62345463fa198f8129cfd12129548eb2
quakkels <quakkels@gmail.com>
2016-10-09 17:57:05 -0500

    Added member navigation and pointing controller, model, and views.

---
+++ src/PointingPoker/Controllers/PointinatingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PointingPoker.Models;
+using System;
+
+namespace PointingPoker.Controllers
+{
+    public class PointinatingController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Card(Guid currentUserId)
+        {
+            return View(new CardInfoViewModel());
+        }
+
+        [HttpPost]
+        public ViewResult Card(CardInfoViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ src/PointingPoker/Models/CardInfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PointingPoker.Models
+{
+    public class CardInfoViewModel
+    {
+        [Required]
+        public Guid CreatedBy { get; set; }
+
+        [Required, MaxLength(512)]
+        public string Description { get; set; }
+
+        public bool IsPointingClosed { get; set; }
+    }
+}
